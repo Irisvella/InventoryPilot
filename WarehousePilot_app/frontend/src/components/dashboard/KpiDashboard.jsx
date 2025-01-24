1b4b774ab17157b22d1d079eb6bcda030c744002
yousefenein <37770446+yousefenein@users.noreply.github.com>
2025-01-24 00:28:22 -0500

    Revert "List qa view"

--- WarehousePilot_app/frontend/src/components/dashboard/KpiDashboard.jsx
+++ WarehousePilot_app/frontend/src/components/dashboard/KpiDashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+// import Sidebar from "../dashboard_sidebar/Sidebar";
 import Header from "../dashboard_sidebar/Header";
 import CycleTime from "./CycleTime";
 import OrderPickingAccuracy from "./OrderPickingAccuracy";
@@ -6,6 +7,8 @@ import StockLevels from "./StockLevels";
 import WarehouseThroughput from "./WarehouseThroughput";
 import OrderFulfillmentRate from "./OrderFulfillmentRate";
 import StockLevelsPreview from "./StockLevelsPreview";
+
+
 import SideBar from "../dashboard_sidebar1/App";
 
 const KPIDashboard = ({ userData }) => {
@@ -16,14 +19,15 @@ const KPIDashboard = ({ userData }) => {
   };
 
   return (
-    <div className="flex min-h-screen bg-gray-100">
+    <div className="flex bg-gray-100">
       {/* Sidebar */}
       <SideBar userData={userData} isOpen={isSidebarOpen} />
 
       {/* Main Content */}
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 sm:ml-10 sm:mt-5">
+
         {/* KPI Dashboard Content */}
-        <main className="flex-1 p-6">
+        <main className="p-6 bg-gray-100 h-screen">
           {/* Title */}
           <h1 className="text-3xl font-bold text-center mb-6">KPI Dashboard</h1>
 
@@ -43,3 +47,4 @@ const KPIDashboard = ({ userData }) => {
 };
 
 export default KPIDashboard;
+
