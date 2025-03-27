2e0f5bda83dce284907584fc23eb1b7dd88b21f6
iluvpesto <n_hassoun3@hotmail.com>
2025-03-27 13:39:11 -0400

    Creating Logs page

---
+++ WarehousePilot_app/frontend/src/components/Inventory_Picking_Logs/InventoryPickingLogs.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function InventoryTable() {
+    return (
+        <div className="mt-2">
+         
+         <h1>TEST</h1>
+          
+        </div>
+      );
+}
